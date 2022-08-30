0f83204affa34973f722a14b5e3a4c5415b9630f
Влад Таланов <111172960+VladTalanov@users.noreply.github.com>
2022-08-30 20:09:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Write("Введите первое число: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int y = int.Parse(Console.ReadLine());
+
+int square = x * x;
+
+if(square == y)
+{
+    Console.WriteLine($"Число {y} это квадрат числа {x}");
+}
+else
+{
+    Console.WriteLine($"Число {y} это не квадрат числа {x}");
+}
